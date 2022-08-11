b4ba19e70d3471f99d08ebfd245a11c2b24e32a9
aScHEABL <natothun@gmail.com>
2022-08-11 17:28:35 +0800

    update: target DOM elements

--- src/modules/inbox.js
+++ src/modules/inbox.js
@@ -8,5 +8,27 @@ import calendarMonthIcon from '../images/calendar-month.png'
 
 export default function createInboxPanel () {
     console.log(`This is the inbox panel`);
- 
+    
+    const inboxContent_DOM = document.querySelector(`[data-inbox-content]`);
+    const inboxBgoverlay_DOM = document.querySelector(`[data-bg-overlay]`);
+    const inboxHeader_DOM = document.querySelector(`[data-inbox-header]`);
+    const toDoListLogo_DOM = document.querySelector(`[data-header-logo]`);
+    const toDoListH1_DOM = document.querySelector(`[data-header-h1]`);
+    const inboxSidebar_DOM = document.querySelector(`[data-inbox-sidebar]`);
+    const upcomingEventsDiv_DOM = document.querySelector(`[data-upcoming-events-div]`);
+    const sidebarDateButtonArray_DOM = document.querySelectorAll(`[data-sidebar-button]`);
+    const main_DOM = document.querySelector(`main`);
+    const sidebarProjectDiv_DOM = document.querySelector(`[data-sidebar-project-div]`);
+    const sidebarProjectH2_DOM = document.querySelector(`[data-sidebar-project-h2]`);
+    const sidebarAddProjectButton_DOM = document.querySelector(`#sidebar-add-project-button`);
+    const sidebarAddprojectIcon_DOM = document.querySelector(`[data-sidebar-add-project-icon]`);
+    const listTitleH1_DOM = document.querySelector(`[data-list-title-h1]`);
+    const addTaskButton_DOM = document.querySelector(`[data-add-task-button]`);
+    const addTaskIcon_DOM = document.querySelector(`[data-add-task-icon]`);
+
+    sidebarDateButtonArray.forEach((button) => {
+        button.addEventListener(`click`, (e) => {
+            
+        })
+    })
 }
