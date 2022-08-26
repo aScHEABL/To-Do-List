6272c987d7fd5a371f7c45fec4ea088c2fc0f7d7
aScHEABL <natothun@gmail.com>
2022-08-26 15:33:13 +0800

    update: tab & task switch eventListeners

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import eventListeners from "./modules/event-listeners.js";
+import "./style.css";
+
+// Images
+
+import toDoList_logo from "./images/check-all.png";
+import inbox_icon from "./images/inbox.png";
+import today_icon from "./images/calendar-today.png";
+import week_icon from "./images/calendar-week.png";
+import month_icon from "./images/calendar-month.png";
+import plus_icon from "./images/plus.png";
+
+// Current page, 0 = Inbox(default), 1 = Today, 2 = Week, 3 = Month
+
+export let currentPage = 0;
+
+// Sidebar tabs
+
+export const sidebarInboxTab_DOM = document.querySelector(`[data-sidebar-inbox-button]`);
+export const sidebarTodayTab_DOM = document.querySelector(`[data-sidebar-today-button]`);
+export const sidebarWeekTab_DOM = document.querySelector(`[data-sidebar-week-button]`);
+export const sidebarMonthTab_DOM = document.querySelector(`[data-sidebar-month-button]`);
+
+// Task buttons
+
+export const main_DOM = document.querySelector('main');
+export const addTaskButton_DOM = document.querySelector('[data-add-task-button]');
+export const taskList_DOM = document.querySelector('[data-task-list]');
+export const addTaskPopupDiv_DOM = document.createElement('div');
+export const addTaskPopupButtonsDiv_DOM = document.createElement('div');
+export const taskAddConfirmButton_DOM = document.createElement('button');
+export const taskCancelButton_DOM = document.createElement('button');
+export const taskInput_DOM = document.createElement('input');
+
+// Icons
+
+const toDoListLogo = document.querySelector(`[data-header-logo]`);
+const inboxIcon = document.querySelector(`[data-inbox-icon]`);
+const todayIcon = document.querySelector(`[data-today-icon]`);
+const weekIcon = document.querySelector(`[data-week-icon]`);
+const monthIcon = document.querySelector(`[data-month-icon]`);
+const sidebarAddProjectIcon = document.querySelector(`[data-sidebar-add-project-icon]`);
+const addTaskIcon = document.querySelector(`[data-add-task-icon]`);
+
+// Icon src
+
+toDoListLogo.src = toDoList_logo;
+inboxIcon.src = inbox_icon;
+todayIcon.src = today_icon;
+weekIcon.src = week_icon;
+monthIcon.src = month_icon;
+sidebarAddProjectIcon.src = plus_icon;
+addTaskIcon.src = plus_icon;
+
+// Active functions
+// inboxPage();
+eventListeners();
---
+++ src/modules/event-listeners.js
@@ -0,0 +1,83 @@
+// Sidebar tabs
+import { sidebarInboxTab_DOM, sidebarTodayTab_DOM, sidebarWeekTab_DOM, sidebarMonthTab_DOM } from "../index.js";
+// Task buttons
+import { main_DOM, addTaskButton_DOM, taskList_DOM, addTaskPopupDiv_DOM, addTaskPopupButtonsDiv_DOM, taskAddConfirmButton_DOM, 
+    taskCancelButton_DOM, taskInput_DOM } from '../index.js';
+
+import { currentPage } from "../index.js";
+
+export default function eventListeners () {
+
+    // Sidebar tabs
+
+    sidebarInboxTab_DOM.addEventListener(`click`, () => {
+        console.log(`Switching to inbox page`);
+        currentPage = 0;
+    })
+
+    sidebarTodayTab_DOM.addEventListener(`click`, () => {
+        console.log(`Switching to today page`);
+        currentPage = 1;
+    })
+
+    sidebarWeekTab_DOM.addEventListener(`click`, () => {
+        console.log(`Switching to week page`);
+        currentPage = 2;
+    })
+
+    sidebarMonthTab_DOM.addEventListener(`click`, () => {
+        console.log(`Switching to month page`);
+        currentPage = 3;
+    })
+
+    // Task buttons
+
+    addTaskButton_DOM.addEventListener(`click`, () => {
+        console.log(`Add task button has been clicked`);
+
+        addTaskButton_DOM.style.display = 'none';
+
+        taskInput_DOM.value = ``;
+
+        addTaskPopupDiv_DOM.classList.add('add-task-popup-div');
+        addTaskPopupButtonsDiv_DOM.classList.add('add-task-popup-buttons-div');
+        taskInput_DOM.classList.add('task-input');
+        taskAddConfirmButton_DOM.classList.add('task-add-confirm-button');
+        taskCancelButton_DOM.classList.add('task-cancel-confirm-button');
+
+        taskAddConfirmButton_DOM.textContent = 'Add';
+        taskCancelButton_DOM.textContent = 'Cancel';
+
+        main_DOM.append(addTaskPopupDiv_DOM);
+        addTaskPopupDiv_DOM.append(taskInput_DOM, addTaskPopupButtonsDiv_DOM);
+        addTaskPopupButtonsDiv_DOM.append(taskAddConfirmButton_DOM, taskCancelButton_DOM);
+    })
+
+    taskAddConfirmButton_DOM.addEventListener('click', () => {
+        console.log('Add task confirm button has been clicked');
+
+
+        addTaskButton_DOM.style.display = 'flex';
+
+        addTaskPopupDiv_DOM.replaceChildren();
+        main_DOM.removeChild(addTaskPopupDiv_DOM);
+
+        if (taskInput_DOM.value.length === 0) {
+            alert("Task name can't be empty!");
+            return;
+        }
+
+
+    })
+
+    taskCancelButton_DOM.addEventListener('click', () => {
+        console.log('Cancel task confirm button has been clicked');
+
+        addTaskButton_DOM.style.display = 'flex';
+
+        addTaskPopupDiv_DOM.replaceChildren();
+        main_DOM.removeChild(addTaskPopupDiv_DOM);
+    })
+
+
+}
